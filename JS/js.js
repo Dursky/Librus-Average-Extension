e52c18b683e43fbe2e2e4049d32ac03f231e373d
WounderDragon <46166716+WounderDragon@users.noreply.github.com>
2021-03-01 11:46:46 +0100

    change name of array, add comments

--- JS/js.js
+++ JS/js.js
@@ -2,22 +2,22 @@ function isFloat(n){return Number(n) === n && n % 1 !== 0;}//Check if number is 
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
-      var array_price = []
+      var grades = []
       var doc = new DOMParser().parseFromString(request.source, "text/html")
         chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
+        //Check if page can get grades.
         if(url.includes("przegladaj_oceny")){                      
             var length_elements =doc.body.querySelectorAll('td.right').length;
-            console.log(length_elements);
             for (var i = 0; i <length_elements;i++){
-                var single_element =doc.body.querySelectorAll('td.right')[i].textContent;
+              var single_element =doc.body.querySelectorAll('td.right')[i].textContent;//Get a iteration element - grades
                var floats = parseFloat(single_element)
                if (floats !== parseInt(floats, 10)){
-               if(isFloat(floats)==true){
-                array_price.push(floats)
+                if (isFloat(floats)==true){
+                  grades.push(floats)
                }}
             }
-            message.innerText = ((array_price.reduce((a, b) => a + b, 0))/array_price.length).toFixed(3);
+            message.innerText = ((grades.reduce((a, b) => a + b, 0))/grades.length).toFixed(3);
         }       
     });
     }
